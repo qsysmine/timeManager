ff8a50546639a2d573ce9440e64dfc97a8c853c5
qsysmine <echomanari@gmail.com>
2014-08-18 20:56:10 -0700

    Update main.lua

--- timeManager/main.lua
+++ timeManager/main.lua
@@ -134,9 +134,9 @@ onLoop = function()
 			end
 		end
 		if isWork then
-			tLT.text = timeLeft.." [Work]"
+			tLT.text = (timeLeft / 60 - timeLeft % 60).." [Work]"
 		else
-			tLT.text = timeLeft.." [Relaxation]"
+			tLT.text = (timeLeft / 60 - timeLeft % 60).." [Relaxation]"
 		end
 	end
 	delay()
